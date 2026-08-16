d161eee275b95139cb28a172f6a12586d7dec2c7
himanshu-flance <himanshu.flance@gmail.com>
2026-08-16 22:42:03 +0530

    First commit

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "This is a latest Demo Welcome to Demo Application";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello DevOps!";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "Application is running";
+    }
+}
